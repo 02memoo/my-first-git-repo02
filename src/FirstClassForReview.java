9a9b9aa1e464acb0232b0445f2c84e4d0e29c56f
02memoo <makbaba0207@gmail.com>
2022-06-23 23:03:34 -0700

    update code

--- src/FirstClassForReview.java
+++ src/FirstClassForReview.java
@@ -1,5 +1,7 @@
 public class FirstClassForReview {
     public static void main(String[] args) {
         System.out.println("Mehmet Akbaba");
+        System.out.println("adding a new code ");
+        System.out.println("this is an another code ");
     }
 }
